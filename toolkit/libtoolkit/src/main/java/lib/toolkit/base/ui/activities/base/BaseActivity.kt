572c1d7e78271224dcd98a3d340f8bf414024462
Michael Efremov <mikhail.efremov@onlyoffice.com>
2021-05-26 18:58:17 +0300

    [android] fix bug 50613

--- toolkit/libtoolkit/src/main/java/lib/toolkit/base/ui/activities/base/BaseActivity.kt
+++ toolkit/libtoolkit/src/main/java/lib/toolkit/base/ui/activities/base/BaseActivity.kt
@@ -574,8 +574,8 @@ abstract class BaseActivity : MvpAppCompatActivity(), FragmentManager.OnBackStac
         KeyboardUtils.hideKeyboard(view)
     }
 
-    protected open fun hideKeyboard(token: IBinder?) {
-        KeyboardUtils.hideKeyboard(this, token)
+    protected open fun hideKeyboard(token: IBinder? = null) {
+        KeyboardUtils.hideKeyboard(this, token ?: window.decorView.windowToken)
     }
 
 }
