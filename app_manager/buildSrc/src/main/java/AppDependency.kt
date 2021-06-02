3af286736736e63f0cf675590820b908de6aa84d
Michael Efremov <mikhail.efremov@onlyoffice.com>
2021-06-02 12:05:00 +0300

    [android] Create buildSrc

---
+++ app_manager/buildSrc/src/main/java/AppDependency.kt
@@ -0,0 +1,8 @@
+object AppDependency {
+    const val COMPILE_SDK_VERSION = 30
+    const val MIN_SDK_VERSION = 23
+    const val TARGET_SDK_VERSION = 30
+    const val BUILD_TOOLS_VERSION = "30.0.3"
+
+    const val NDK_VERSION = "21.3.6528147"
+}
