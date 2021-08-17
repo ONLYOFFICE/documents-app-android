aec1787df7df8891d2cae4c832c3284d92dbc1c1
Michael Efremov <mikhail.efremov@onlyoffice.com>
2021-08-17 12:18:09 +0300

    [android] Check night mode

--- toolkit/libtoolkit/src/main/java/lib/toolkit/base/managers/utils/UiUtils.kt
+++ toolkit/libtoolkit/src/main/java/lib/toolkit/base/managers/utils/UiUtils.kt
@@ -685,6 +685,15 @@ object UiUtils {
             .show()
     }
 
+    fun isDarkMode(context: Context): Boolean {
+        return when (context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> true
+            Configuration.UI_MODE_NIGHT_NO -> false
+            Configuration.UI_MODE_NIGHT_UNDEFINED -> false
+            else -> false
+        }
+    }
+
 }
 
 
