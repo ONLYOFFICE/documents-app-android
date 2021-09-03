c197dc669e11b6c65c445b68072880e918169ae9
Sergey Artyushkin <sergey.artyushkin@onlyoffice.com>
2021-09-03 12:17:48 +0300

    [android] Remove ShareAddAdapter

--- app_manager/appmanager/src/main/java/app/editors/manager/ui/adapters/share/ShareAdapter.kt
+++ app_manager/appmanager/src/main/java/app/editors/manager/ui/adapters/share/ShareAdapter.kt
@@ -2,12 +2,29 @@ package app.editors.manager.ui.adapters.share
 
 import app.editors.manager.mvp.models.ui.ShareHeaderUi
 import app.editors.manager.ui.adapters.BaseViewTypeAdapter
+import app.editors.manager.ui.adapters.holders.ShareViewHolder
 import app.editors.manager.ui.adapters.holders.factory.ShareHolderFactory
+import lib.toolkit.base.ui.adapters.BaseAdapter
+import lib.toolkit.base.ui.adapters.holder.BaseViewHolder
 import lib.toolkit.base.ui.adapters.holder.ViewType
 
-class ShareAdapter(
-    factory: ShareHolderFactory,
-) : BaseViewTypeAdapter<ViewType>(factory) {
+class ShareAdapter(factory: ShareHolderFactory) : BaseViewTypeAdapter<ViewType>(factory) {
+
+    private var mode: BaseAdapter.Mode = BaseAdapter.Mode.USERS
+
+    override fun onBindViewHolder(holder: BaseViewHolder<ViewType>, position: Int) {
+        if (holder is ShareViewHolder<ViewType>) {
+            getItem(position)?.let { item ->
+                holder.bind(item, mode, getItem(position - 1))
+            }
+        } else {
+            super.onBindViewHolder(holder, position)
+        }
+    }
+
+    fun setMode(mode: BaseAdapter.Mode) {
+        this.mode = mode
+    }
 
     fun removeHeader(item: ShareHeaderUi) {
         removeItem(item)
