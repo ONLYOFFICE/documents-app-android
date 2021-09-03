f509239730b025278323eac4d5106d3b2bec4c4a
Sergey Artyushkin <sergey.artyushkin@onlyoffice.com>
2021-09-03 17:07:12 +0300

    [android] Refactor BaseAppFragment

--- app_manager/appmanager/src/main/java/app/editors/manager/ui/fragments/base/BaseAppFragment.kt
+++ app_manager/appmanager/src/main/java/app/editors/manager/ui/fragments/base/BaseAppFragment.kt
@@ -1,117 +1,108 @@
-package app.editors.manager.ui.fragments.base;
+package app.editors.manager.ui.fragments.base
 
-import android.content.Context;
-import android.view.Menu;
-import android.view.MenuInflater;
+import android.content.Context
+import android.view.Menu
+import android.view.MenuInflater
+import androidx.fragment.app.Fragment
+import app.editors.manager.R
+import app.editors.manager.mvp.models.explorer.CloudFile
+import app.editors.manager.mvp.models.explorer.Explorer
+import app.editors.manager.mvp.models.explorer.Item
+import app.editors.manager.ui.activities.base.BaseAppActivity
+import app.editors.manager.ui.activities.main.MediaActivity.Companion.show
+import app.editors.manager.ui.activities.main.OperationActivity.Companion.showCopy
+import app.editors.manager.ui.activities.main.OperationActivity.Companion.showMove
+import app.editors.manager.ui.activities.main.ShareActivity.Companion.show
+import app.editors.manager.ui.activities.main.StorageActivity.Companion.show
+import app.editors.manager.ui.activities.main.WebViewerActivity.Companion.show
+import app.editors.manager.ui.interfaces.ContextDialogInterface
+import lib.toolkit.base.managers.utils.FragmentUtils.showFragment
+import lib.toolkit.base.ui.fragments.base.BaseFragment
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+abstract class BaseAppFragment : BaseFragment() {
 
-import app.editors.manager.R;
-import app.editors.manager.mvp.models.explorer.Explorer;
-import app.editors.manager.mvp.models.explorer.CloudFile;
-import app.editors.manager.mvp.models.explorer.Item;
-import app.editors.manager.ui.activities.base.BaseAppActivity;
-import app.editors.manager.ui.activities.main.MediaActivity;
-import app.editors.manager.ui.activities.main.OperationActivity;
-import app.editors.manager.ui.activities.main.ShareActivity;
-import app.editors.manager.ui.activities.main.StorageActivity;
-import app.editors.manager.ui.activities.main.WebViewerActivity;
-import app.editors.manager.ui.interfaces.ContextDialogInterface;
-import lib.toolkit.base.managers.utils.FragmentUtils;
-import lib.toolkit.base.ui.fragments.base.BaseFragment;
+    var mContextDialogListener: ContextDialogInterface? = null
+    protected var menu: Menu? = null
+    protected var menuInflater: MenuInflater? = null
+    private val TAG = javaClass.simpleName
 
-public abstract class BaseAppFragment extends BaseFragment {
-
-    private final String TAG = getClass().getSimpleName();
-
-    protected static final int PERMISSION_SMS = 0;
-    protected static final int PERMISSION_WRITE_STORAGE = 1;
-    protected static final int PERMISSION_READ_STORAGE = 2;
-    protected static final int PERMISSION_CAMERA = 3;
-    protected static final int PERMISSION_READ_UPLOAD = 4;
-
-    @Nullable
-    protected Menu mMenu;
-
-    @Nullable
-    protected MenuInflater mMenuInflater;
-
-    public ContextDialogInterface mContextDialogListener;
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
         try {
-            mContextDialogListener = (ContextDialogInterface) context;
-            addOnDispatchTouchEvent();
-        } catch (ClassCastException e) {
-            throw new RuntimeException(BaseAppFragment.class.getSimpleName() + " - must implement - " +
-                    BaseAppActivity.class.getSimpleName());
+            mContextDialogListener = context as ContextDialogInterface
+            addOnDispatchTouchEvent()
+        } catch (e: ClassCastException) {
+            throw RuntimeException(
+                BaseAppFragment::class.java.simpleName + " - must implement - " +
+                        BaseAppActivity::class.java.simpleName
+            )
         }
     }
 
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        mMenu = menu;
-        mMenuInflater = inflater;
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        this.menu = menu
+        menuInflater = inflater
     }
 
-    @Override
-    public boolean onBackPressed() {
-        return false;
+    override fun onBackPressed(): Boolean {
+        return false
     }
 
-
-    /*
+    /**
      * Fragment operations
      * */
-    protected void showFragment(@NonNull final Fragment fragment, @Nullable final String tag, boolean isAdd) {
-        if (getFragmentManager() != null) {
-            FragmentUtils.showFragment(getFragmentManager(), fragment, R.id.frame_container, tag, isAdd);
+    protected fun showFragment(fragment: Fragment, tag: String?, isAdd: Boolean) {
+        fragmentManager?.let {
+            showFragment(it, fragment, R.id.frame_container, tag, isAdd)
         }
     }
 
-    protected void showParentFragment(@NonNull final Fragment fragment, @Nullable final String tag, boolean isAdd) {
-        if (getParentFragment() != null && getParentFragment().getFragmentManager() != null) {
-            FragmentUtils.showFragment(getParentFragment().getFragmentManager(), fragment, R.id.frame_container, tag, isAdd);
+    protected fun showParentFragment(fragment: Fragment, tag: String?, isAdd: Boolean) {
+        parentFragment?.fragmentManager?.let {
+            showFragment(it, fragment, R.id.frame_container, tag, isAdd)
         }
     }
 
-    /*
+    /**
      * Keyboard
      * */
-    protected void copySharedLinkToClipboard(@Nullable final String value, @Nullable final String message) {
-        copyToClipboard(getString(R.string.share_clipboard_external_link_label), value, message);
+    protected fun copySharedLinkToClipboard(value: String?, message: String?) {
+        copyToClipboard(getString(R.string.share_clipboard_external_link_label), value, message)
     }
 
-    /*
+    /**
      * Show activity
      * */
-    protected void showOperationMoveActivity(@NonNull Explorer explorer) {
-        OperationActivity.showMove(this, explorer);
+    protected fun showOperationMoveActivity(explorer: Explorer) {
+        showMove(this, explorer)
     }
 
-    protected void showOperationCopyActivity(@NonNull Explorer explorer) {
-        OperationActivity.showCopy(this, explorer);
+    protected fun showOperationCopyActivity(explorer: Explorer) {
+        showCopy(this, explorer)
     }
 
-    protected void showViewerActivity(final CloudFile file) {
-        WebViewerActivity.show(requireActivity(), file);
+    protected fun showViewerActivity(file: CloudFile?) {
+        show(requireActivity(), file)
     }
 
-    protected void showMediaActivity(final Explorer explorer, final boolean isWebDAv) {
-        MediaActivity.show(this, explorer, isWebDAv);
+    protected fun showMediaActivity(explorer: Explorer?, isWebDAv: Boolean) {
+        show(this, explorer, isWebDAv)
     }
 
-    protected void showShareActivity(final Item item) {
-        ShareActivity.show(this, item);
+    protected fun showShareActivity(item: Item?) {
+        show(this, item)
     }
 
-    protected void showStorageActivity(final boolean isMySection) {
-        StorageActivity.show(this, isMySection);
+    protected fun showStorageActivity(isMySection: Boolean) {
+        show(this, isMySection)
     }
 
+    companion object {
+        protected const val PERMISSION_SMS = 0
+        protected const val PERMISSION_WRITE_STORAGE = 1
+        protected const val PERMISSION_READ_STORAGE = 2
+        protected const val PERMISSION_CAMERA = 3
+        protected const val PERMISSION_READ_UPLOAD = 4
+    }
 }
