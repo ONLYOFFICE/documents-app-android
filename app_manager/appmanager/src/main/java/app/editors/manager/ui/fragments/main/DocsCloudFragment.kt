0aeff25eb532faeb78718cfd8c7eb3fc4b9d46cc
Sergey Artyushkin <sergey.artyushkin@onlyoffice.com>
2021-09-06 11:32:44 +0300

    [android] Refactor DocsCloudFragment

--- app_manager/appmanager/src/main/java/app/editors/manager/ui/fragments/main/DocsCloudFragment.kt
+++ app_manager/appmanager/src/main/java/app/editors/manager/ui/fragments/main/DocsCloudFragment.kt
@@ -1,292 +1,241 @@
-package app.editors.manager.ui.fragments.main;
+package app.editors.manager.ui.fragments.main
 
-import android.app.Activity;
-import android.content.Intent;
+import android.app.Activity
+import android.content.Intent
+import app.documents.core.network.ApiContract
+import app.editors.manager.R
+import app.editors.manager.app.App.Companion.getApp
+import app.editors.manager.mvp.models.base.Entity
+import app.editors.manager.mvp.models.explorer.CloudFile
+import app.editors.manager.mvp.models.explorer.CloudFolder
+import app.editors.manager.mvp.models.list.Header
+import app.editors.manager.mvp.presenters.main.DocsBasePresenter
+import app.editors.manager.mvp.presenters.main.DocsCloudPresenter
+import app.editors.manager.mvp.views.main.DocsBaseView
+import app.editors.manager.mvp.views.main.DocsCloudView
+import app.editors.manager.ui.activities.main.ShareActivity
+import app.editors.manager.ui.activities.main.StorageActivity
+import app.editors.manager.ui.dialogs.ActionBottomDialog
+import app.editors.manager.ui.dialogs.ContextBottomDialog
+import app.editors.manager.ui.dialogs.MoveCopyDialog
+import app.editors.manager.ui.dialogs.MoveCopyDialog.Companion.newInstance
+import lib.toolkit.base.managers.utils.TimeUtils.fileTimeStamp
+import lib.toolkit.base.managers.utils.UiUtils.setMenuItemTint
+import lib.toolkit.base.ui.activities.base.BaseActivity
+import lib.toolkit.base.ui.dialogs.common.CommonDialog.Dialogs
+import moxy.presenter.InjectPresenter
+import moxy.presenter.ProvidePresenter
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import app.documents.core.account.CloudAccount;
-import app.documents.core.network.ApiContract;
-import app.editors.manager.R;
-import app.editors.manager.app.App;
-import app.editors.manager.mvp.models.base.Entity;
-import app.editors.manager.mvp.models.explorer.CloudFile;
-import app.editors.manager.mvp.models.explorer.CloudFolder;
-import app.editors.manager.mvp.models.explorer.UploadFile;
-import app.editors.manager.mvp.models.list.Header;
-import app.editors.manager.mvp.presenters.main.DocsBasePresenter;
-import app.editors.manager.mvp.presenters.main.DocsCloudPresenter;
-import app.editors.manager.mvp.views.main.DocsBaseView;
-import app.editors.manager.mvp.views.main.DocsCloudView;
-import app.editors.manager.ui.activities.base.BaseAppActivity;
-import app.editors.manager.ui.activities.main.ShareActivity;
-import app.editors.manager.ui.activities.main.StorageActivity;
-import app.editors.manager.ui.dialogs.ActionBottomDialog;
-import app.editors.manager.ui.dialogs.ContextBottomDialog;
-import app.editors.manager.ui.dialogs.MoveCopyDialog;
-import lib.toolkit.base.managers.utils.TimeUtils;
-import lib.toolkit.base.managers.utils.UiUtils;
-import lib.toolkit.base.ui.dialogs.common.CommonDialog;
-import moxy.presenter.InjectPresenter;
-import moxy.presenter.ProvidePresenter;
-
-public abstract class DocsCloudFragment extends DocsBaseFragment implements DocsCloudView {
-
-    protected static String KEY_ACCOUNT = "key_account";
+abstract class DocsCloudFragment : DocsBaseFragment(), DocsCloudView {
 
     @InjectPresenter
-    public DocsCloudPresenter mCloudPresenter;
+    lateinit var cloudPresenter: DocsCloudPresenter
 
     @ProvidePresenter
-    public DocsCloudPresenter providePresenter() {
-        CloudAccount account = App.getApp().getAppComponent().getAccountOnline();
-        if (account != null) {
-            return new DocsCloudPresenter(account);
-        } else throw new RuntimeException("Cloud account can't be null");
+    fun providePresenter(): DocsCloudPresenter {
+        val account = getApp().appComponent.accountOnline
+        return account?.let { DocsCloudPresenter(it) }
+            ?: throw RuntimeException("Cloud account can't be null")
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_OK) {
-            switch (requestCode) {
-                case BaseAppActivity.REQUEST_ACTIVITY_WEB_VIEWER: {
-                    showViewerActivity((CloudFile) data.getSerializableExtra("TAG_FILE"));
-                    break;
+            when (requestCode) {
+                BaseActivity.REQUEST_ACTIVITY_WEB_VIEWER -> {
+                    showViewerActivity(data!!.getSerializableExtra("TAG_FILE") as CloudFile?)
                 }
-
-                case BaseAppActivity.REQUEST_ACTIVITY_OPERATION: {
-                    showSnackBar(R.string.operation_complete_message);
-                    onRefresh();
-                    break;
+                BaseActivity.REQUEST_ACTIVITY_OPERATION -> {
+                    showSnackBar(R.string.operation_complete_message)
+                    onRefresh()
                 }
-                case BaseAppActivity.REQUEST_ACTIVITY_STORAGE: {
-                    final CloudFolder folder = (CloudFolder) data.getSerializableExtra(StorageActivity.TAG_RESULT);
-                    mCloudPresenter.addFolderAndOpen(folder, mLinearLayoutManager.findFirstVisibleItemPosition());
-                    break;
+                BaseActivity.REQUEST_ACTIVITY_STORAGE -> {
+                    val folder =
+                        data?.getSerializableExtra(StorageActivity.TAG_RESULT) as CloudFolder?
+                    cloudPresenter.addFolderAndOpen(
+                        folder,
+                        linearLayoutManager!!.findFirstVisibleItemPosition()
+                    )
                 }
-                case BaseAppActivity.REQUEST_ACTIVITY_SHARE: {
-                    if (data != null && data.hasExtra(ShareActivity.TAG_RESULT)) {
-                        mCloudPresenter.setItemsShared(data.getBooleanExtra(ShareActivity.TAG_RESULT, false));
+                BaseActivity.REQUEST_ACTIVITY_SHARE -> {
+                    if (data?.hasExtra(ShareActivity.TAG_RESULT) == true) {
+                        cloudPresenter.setItemsShared(data
+                            .getBooleanExtra(ShareActivity.TAG_RESULT, false))
                     }
-                    break;
                 }
-                case BaseAppActivity.REQUEST_ACTIVITY_CAMERA: {
-                    if (mCameraUri != null) {
-                        mCloudPresenter.upload(mCameraUri, null);
+                BaseActivity.REQUEST_ACTIVITY_CAMERA -> {
+                    mCameraUri?.let { uri ->
+                        cloudPresenter.upload(uri, null)
                     }
-                    break;
                 }
-                case BaseAppActivity.REQUEST_ACTIVITY_FILE_PICKER: {
-                    if (data != null && data.getClipData() != null) {
-                        mCloudPresenter.upload(null, data.getClipData());
+                BaseActivity.REQUEST_ACTIVITY_FILE_PICKER -> {
+                    data?.clipData?.let {
+                        cloudPresenter.upload(null, it)
                     }
-                    if (data != null && data.getData() != null) {
-                        mCloudPresenter.upload(data.getData(), null);
+                    data?.data?.let {
+                        cloudPresenter.upload(it, null)
                     }
-                    break;
                 }
             }
         }
     }
 
-    @Override
-    public void onActionBarTitle(String title) {
-        if (isActivePage()) {
-            setActionBarTitle(title);
-            if (title.equals("0")) {
-                disableMenu();
+    override fun onActionBarTitle(title: String) {
+        if (isActivePage) {
+            setActionBarTitle(title)
+            if (title == "0") {
+                disableMenu()
             }
         }
     }
 
-    @Override
-    public void onStateMenuSelection() {
-        if (mMenu != null && mMenuInflater != null) {
-            mMenuInflater.inflate(R.menu.docs_select, mMenu);
-            mDeleteItem = mMenu.findItem(R.id.toolbar_selection_delete).setVisible(mCloudPresenter.isContextItemEditable());
-            mMoveItem = mMenu.findItem(R.id.toolbar_selection_move).setVisible(mCloudPresenter.isContextItemEditable());
-            mCopyItem = mMenu.findItem(R.id.toolbar_selection_copy);
-            mDownloadItem = mMenu.findItem(R.id.toolbar_selection_download).setVisible(!mCloudPresenter.isTrashMode());
-            UiUtils.setMenuItemTint(requireContext(), mDeleteItem, R.color.colorWhite);
-            setAccountEnable(false);
-        }
-    }
-
-    @Override
-    public void onUploadFileProgress(int progress, String id) {
-        UploadFile uploadFile = mExplorerAdapter.getUploadFileById(id);
-        if(uploadFile != null) {
-            uploadFile.setProgress(progress);
-            mExplorerAdapter.updateItem(uploadFile);
-        }
-    }
-
-    @Override
-    public void onDeleteUploadFile(String id) {
-        mExplorerAdapter.removeUploadItemById(id);
-    }
-
-    @Override
-    public void onRemoveUploadHead() {
-        mExplorerAdapter.removeHeader(App.getApp().getString(R.string.upload_manager_progress_title));
-    }
-
-    @SuppressWarnings("unchecked")
-    @Override
-    public void onAddUploadsFile(List<? extends Entity> uploadFiles) {
-        onRemoveUploadHead();
-        mExplorerAdapter.addItemsAtTop((List<Entity>) uploadFiles);
-        mExplorerAdapter.addItemAtTop(new Header(getString(R.string.upload_manager_progress_title)));
-        mRecyclerView.scrollToPosition(0);
-    }
-
-    @Override
-    public void showMoveCopyDialog(ArrayList<String> names, String action, String titleFolder) {
-        mMoveCopyDialog = MoveCopyDialog.newInstance(names, action, titleFolder);
-        mMoveCopyDialog.setOnClick(this);
-        mMoveCopyDialog.show(requireFragmentManager(), MoveCopyDialog.TAG);
-    }
-
-    @Override
-    public void onActionButtonClick(ActionBottomDialog.Buttons buttons) {
-        super.onActionButtonClick(buttons);
-        switch (buttons) {
-            case PHOTO:
-                if (checkCameraPermission()) {
-                    showCameraActivity(TimeUtils.getFileTimeStamp());
-                }
-                break;
-            case STORAGE: {
-                showStorageActivity(mCloudPresenter.isUserSection());
-                //TODO show user info about third party storage location
+    override fun onStateMenuSelection() {
+        menu?.let { menu ->
+            menuInflater?.let { menuInflater ->
+                menuInflater.inflate(R.menu.docs_select, menu)
+                deleteItem = menu.findItem(R.id.toolbar_selection_delete)
+                    .setVisible(cloudPresenter.isContextItemEditable).also {
+                        setMenuItemTint(requireContext(), it, R.color.colorWhite)
+                    }
+                moveItem = menu.findItem(R.id.toolbar_selection_move)
+                    .setVisible(cloudPresenter.isContextItemEditable)
+                copyItem = menu.findItem(R.id.toolbar_selection_copy)
+                downloadItem = menu.findItem(R.id.toolbar_selection_download)
+                    .setVisible(!cloudPresenter.isTrashMode)
+                setAccountEnable(false)
             }
         }
     }
 
-    @Override
-    public void onAcceptClick(CommonDialog.Dialogs dialogs, @Nullable String value, @Nullable String tag) {
-        super.onAcceptClick(dialogs, value, tag);
-        if (tag != null) {
-            switch (tag) {
-                case DocsBasePresenter.TAG_DIALOG_BATCH_EMPTY:
-                    mCloudPresenter.emptyTrash();
-                    break;
-                case DocsBasePresenter.TAG_DIALOG_CONTEXT_SHARE_DELETE:
-                    mCloudPresenter.removeShareContext();
-                    break;
+    override fun onUploadFileProgress(progress: Int, id: String) {
+        explorerAdapter?.getUploadFileById(id).let { uploadFile ->
+            uploadFile?.progress = progress
+            explorerAdapter?.updateItem(uploadFile)
+        }
+    }
+
+    override fun onDeleteUploadFile(id: String) {
+        explorerAdapter?.removeUploadItemById(id)
+    }
+
+    override fun onRemoveUploadHead() {
+        explorerAdapter?.removeHeader(getApp().getString(R.string.upload_manager_progress_title))
+    }
+
+    override fun onAddUploadsFile(uploadFiles: List<Entity>) {
+        onRemoveUploadHead()
+        explorerAdapter?.addItemsAtTop(uploadFiles)
+        explorerAdapter?.addItemAtTop(Header(getString(R.string.upload_manager_progress_title)))
+        recyclerView?.scrollToPosition(0)
+    }
+
+    override fun showMoveCopyDialog(names: ArrayList<String>, action: String, titleFolder: String) {
+        moveCopyDialog = newInstance(names, action, titleFolder)
+        moveCopyDialog?.dialogButtonOnClick = this
+        moveCopyDialog?.show(requireFragmentManager(), MoveCopyDialog.TAG)
+    }
+
+    override fun onActionButtonClick(buttons: ActionBottomDialog.Buttons?) {
+        super.onActionButtonClick(buttons)
+        when (buttons) {
+            ActionBottomDialog.Buttons.PHOTO -> if (checkCameraPermission()) {
+                showCameraActivity(fileTimeStamp)
+            }
+            ActionBottomDialog.Buttons.STORAGE -> {
+                showStorageActivity(cloudPresenter.isUserSection)
+            }
+            else -> { }
+        }
+    }
+
+    override fun onAcceptClick(dialogs: Dialogs?, value: String?, tag: String?) {
+        super.onAcceptClick(dialogs, value, tag)
+        tag?.let {
+            when (tag) {
+                DocsBasePresenter.TAG_DIALOG_BATCH_EMPTY -> cloudPresenter.emptyTrash()
+                DocsBasePresenter.TAG_DIALOG_CONTEXT_SHARE_DELETE -> cloudPresenter.removeShareContext()
             }
         }
     }
 
-    @Override
-    public void onContextButtonClick(ContextBottomDialog.Buttons buttons) {
-        super.onContextButtonClick(buttons);
-        switch (buttons) {
-            case EDIT:
-                mCloudPresenter.onEditContextClick();
-                break;
-            case SHARE:
-                showShareActivity(mCloudPresenter.getItemClicked());
-                break;
-            case EXTERNAL:
-                setContextDialogExternalLinkEnable(false);
-                mCloudPresenter.getExternalLink();
-                break;
-            case SHARE_DELETE:
-                showQuestionDialog(getString(R.string.dialogs_question_share_remove), mCloudPresenter.getItemTitle(),
-                        getString(R.string.dialogs_question_share_remove), getString(R.string.dialogs_common_cancel_button),
-                        DocsBasePresenter.TAG_DIALOG_CONTEXT_SHARE_DELETE);
-                break;
-            case FAVORITE_ADD:
-                mCloudPresenter.addToFavorite();
-                break;
-            case FAVORITE_DELETE:
-                mCloudPresenter.deleteFromFavorite();
-                break;
-        }
-    }
-
-    @Override
-    public void continueClick(@NonNull String tag, String action) {
-        int operationType = ApiContract.Operation.OVERWRITE;
-        switch (tag) {
-            case MoveCopyDialog.TAG_DUPLICATE:
-                operationType = ApiContract.Operation.DUPLICATE;
-                break;
-            case MoveCopyDialog.TAG_OVERWRITE:
-                operationType = ApiContract.Operation.OVERWRITE;
-                break;
-            case MoveCopyDialog.TAG_SKIP:
-                operationType = ApiContract.Operation.SKIP;
-                break;
-        }
-        if (action.equals(MoveCopyDialog.ACTION_COPY)) {
-            mCloudPresenter.transfer(operationType, false);
-        } else {
-            mCloudPresenter.transfer(operationType, true);
-        }
-    }
-
-    @Override
-    protected boolean isActivePage() {
-            final Fragment fragment = getParentFragment();
-            if (fragment instanceof MainPagerFragment) {
-                return ((MainPagerFragment) fragment).isActivePage(this);
-            } else {
-                return true;
+    override fun onContextButtonClick(buttons: ContextBottomDialog.Buttons?) {
+        super.onContextButtonClick(buttons)
+        when (buttons) {
+            ContextBottomDialog.Buttons.EDIT -> cloudPresenter.onEditContextClick()
+            ContextBottomDialog.Buttons.SHARE -> showShareActivity(
+                cloudPresenter.itemClicked
+            )
+            ContextBottomDialog.Buttons.EXTERNAL -> {
+                setContextDialogExternalLinkEnable(false)
+                cloudPresenter.externalLink
             }
-
+            ContextBottomDialog.Buttons.SHARE_DELETE -> showQuestionDialog(
+                getString(R.string.dialogs_question_share_remove),
+                cloudPresenter.itemTitle,
+                getString(R.string.dialogs_question_share_remove),
+                getString(R.string.dialogs_common_cancel_button),
+                DocsBasePresenter.TAG_DIALOG_CONTEXT_SHARE_DELETE
+            )
+            ContextBottomDialog.Buttons.FAVORITE_ADD -> cloudPresenter.addToFavorite()
+            ContextBottomDialog.Buttons.FAVORITE_DELETE -> cloudPresenter.deleteFromFavorite()
+            else -> { }
+        }
     }
 
-    @Override
-    public void onFileWebView(CloudFile file) {
-        showViewerActivity(file);
+    override fun continueClick(tag: String?, action: String?) {
+        var operationType = ApiContract.Operation.OVERWRITE
+        when (tag) {
+            MoveCopyDialog.TAG_DUPLICATE -> operationType = ApiContract.Operation.DUPLICATE
+            MoveCopyDialog.TAG_OVERWRITE -> operationType = ApiContract.Operation.OVERWRITE
+            MoveCopyDialog.TAG_SKIP -> operationType = ApiContract.Operation.SKIP
+        }
+        cloudPresenter.transfer(operationType, action != MoveCopyDialog.ACTION_COPY)
     }
 
-    @Override
-    protected DocsBasePresenter<? extends DocsBaseView> getPresenter() {
-        return mCloudPresenter;
+    override fun onFileWebView(file: CloudFile) {
+        showViewerActivity(file)
     }
 
     /*
      * On pager scroll callback
      * */
-    public void onScrollPage() {
-        mCloudPresenter.initViews();
+    override fun onScrollPage() {
+        cloudPresenter.initViews()
     }
 
-    private void disableMenu() {
-        if (mMenu != null) {
-            mDeleteItem.setEnabled(false);
+    override fun onResume() {
+        super.onResume()
+        cloudPresenter.setSectionType(section)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        cloudPresenter.setSectionType(section)
+    }
+
+    override val isActivePage: Boolean
+        get() = parentFragment.let {
+                it is MainPagerFragment && it.isActivePage(this)
+            }
+
+    override val presenter: DocsBasePresenter<out DocsBaseView?>
+        get() = cloudPresenter
+
+    override val isWebDav: Boolean
+        get() = false
+
+    protected abstract val section: Int
+
+    private fun disableMenu() {
+        menu?.let {
+            deleteItem?.isEnabled = false
         }
     }
 
-    @Override
-    protected Boolean isWebDav() {
-        return false;
-    }
+    val isRoot: Boolean
+        get() = presenter.isRoot
 
-    public boolean isRoot() {
-        return getPresenter().isRoot();
+    companion object {
+        protected var KEY_ACCOUNT = "key_account"
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        mCloudPresenter.setSectionType(getSection());
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        mCloudPresenter.setSectionType(getSection());
-    }
-
-    abstract protected int getSection();
 }
