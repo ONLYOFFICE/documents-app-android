89269590eebef91c41384c5de1c6802601b585f7
Sergey Artyushkin <sergey.artyushkin@onlyoffice.com>
2021-09-06 19:46:48 +0300

    [android] Refactor OnBoardingPageFragment

--- app_manager/appmanager/src/main/java/app/editors/manager/ui/fragments/onboarding/OnBoardingPageFragment.kt
+++ app_manager/appmanager/src/main/java/app/editors/manager/ui/fragments/onboarding/OnBoardingPageFragment.kt
@@ -1,95 +1,79 @@
-package app.editors.manager.ui.fragments.onboarding;
+package app.editors.manager.ui.fragments.onboarding
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.MotionEvent;
-import android.view.View;
-import android.view.ViewGroup;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import app.editors.manager.databinding.FragmentOnBoardingPageBinding
+import app.editors.manager.ui.fragments.base.BaseAppFragment
 
-import androidx.annotation.DrawableRes;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.StringRes;
-import androidx.appcompat.widget.AppCompatImageView;
-import androidx.appcompat.widget.AppCompatTextView;
-
-import app.editors.manager.R;
-import app.editors.manager.ui.fragments.base.BaseAppFragment;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
-
-public class OnBoardingPageFragment extends BaseAppFragment {
-
-    public static final String TAG = OnBoardingPageFragment.class.getSimpleName();
-    public static final String TAG_HEADER = "TAG_HEADER";
-    public static final String TAG_INFO = "TAG_INFO";
-    public static final String TAG_IMAGE = "TAG_IMAGE";
-
-    private Unbinder mUnbinder;
-    @BindView(R.id.on_boarding_page_header)
-    protected AppCompatTextView mOnBoardingPageHeader;
-    @BindView(R.id.on_boarding_page_info)
-    protected AppCompatTextView mOnBoardingPageInfo;
-    @BindView(R.id.on_boarding_page_image)
-    protected AppCompatImageView mOnBoardingPageImage;
+class OnBoardingPageFragment : BaseAppFragment() {
 
     @StringRes
-    private int mHeaderResId;
+    private var mHeaderResId = 0
+
     @StringRes
-    private int mInfoResId;
+    private var mInfoResId = 0
+
     @DrawableRes
-    private int mImageResId;
+    private var mImageResId = 0
 
-    public static OnBoardingPageFragment newInstance(@StringRes int headerId, @StringRes int infoId, @DrawableRes int imageId) {
-        final OnBoardingPageFragment fragment = new OnBoardingPageFragment();
-        final Bundle bundle = new Bundle();
-        bundle.putInt(TAG_HEADER, headerId);
-        bundle.putInt(TAG_INFO, infoId);
-        bundle.putInt(TAG_IMAGE, imageId);
-        fragment.setArguments(bundle);
-        return fragment;
+    private var viewBinding: FragmentOnBoardingPageBinding? = null
+
+    override fun dispatchTouchEvent(ev: MotionEvent) { }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        viewBinding = FragmentOnBoardingPageBinding.inflate(inflater, container, false)
+        return viewBinding?.root
     }
 
-    @Override
-    public void dispatchTouchEvent(@NonNull MotionEvent ev) {
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
     }
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
-        final View view = inflater.inflate(R.layout.fragment_on_boarding_page, container, false);
-        mUnbinder = ButterKnife.bind(this, view);
-        return view;
+    override fun onDestroyView() {
+        super.onDestroyView()
+        viewBinding = null
     }
 
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        init();
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        mUnbinder.unbind();
-    }
-
-    private void init() {
-        getArgs();
-        mOnBoardingPageHeader.setText(mHeaderResId);
-        mOnBoardingPageInfo.setText(mInfoResId);
-        mOnBoardingPageImage.setImageResource(mImageResId);
-    }
-
-    private void getArgs() {
-        final Bundle bundle = getArguments();
-        if (bundle != null) {
-            mHeaderResId = bundle.getInt(TAG_HEADER);
-            mInfoResId = bundle.getInt(TAG_INFO);
-            mImageResId = bundle.getInt(TAG_IMAGE);
+    private fun init() {
+        getArgs()
+        viewBinding?.let {
+            it.onBoardingPageHeader.setText(mHeaderResId)
+            it.onBoardingPageInfo.setText(mInfoResId)
+            it.onBoardingPageImage.setImageResource(mImageResId)
         }
     }
 
+    private fun getArgs() {
+        arguments?.let { bundle ->
+            mHeaderResId = bundle.getInt(TAG_HEADER)
+            mInfoResId = bundle.getInt(TAG_INFO)
+            mImageResId = bundle.getInt(TAG_IMAGE)
+        }
+    }
+
+    companion object {
+        val TAG = OnBoardingPageFragment::class.java.simpleName
+        const val TAG_HEADER = "TAG_HEADER"
+        const val TAG_INFO = "TAG_INFO"
+        const val TAG_IMAGE = "TAG_IMAGE"
+
+        fun newInstance(@StringRes headerId: Int, @StringRes infoId: Int, @DrawableRes imageId: Int):
+                OnBoardingPageFragment = OnBoardingPageFragment().apply {
+                arguments = Bundle(3).apply {
+                    putInt(TAG_HEADER, headerId)
+                    putInt(TAG_INFO, infoId)
+                    putInt(TAG_IMAGE, imageId)
+                }
+            }
+        }
 }
