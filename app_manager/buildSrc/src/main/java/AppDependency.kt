1b7cd3dfbddf4344464689e59135f593e0745794
Michael Efremov <mikhail.efremov@onlyoffice.com>
2021-09-16 18:39:23 +0300

    [android][all] Migrate version gradle plugin to 7

--- app_manager/buildSrc/src/main/java/AppDependency.kt
+++ app_manager/buildSrc/src/main/java/AppDependency.kt
@@ -2,7 +2,7 @@ object AppDependency {
     const val COMPILE_SDK_VERSION = 30
     const val MIN_SDK_VERSION = 23
     const val TARGET_SDK_VERSION = 30
-    const val BUILD_TOOLS_VERSION = "30.0.3"
+    const val BUILD_TOOLS_VERSION = "31.0.0"
 
     const val NDK_VERSION = "21.3.6528147"
 
