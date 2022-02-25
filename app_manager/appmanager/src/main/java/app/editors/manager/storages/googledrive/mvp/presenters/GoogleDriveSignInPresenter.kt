b9a4f87881f4009dec6571896ca474f65b973a6e
Michael Efremov <mikhail.efremov@onlyoffice.com>
2022-02-25 17:18:12 +0300

    [android] Fix import

--- app_manager/appmanager/src/main/java/app/editors/manager/storages/googledrive/mvp/presenters/GoogleDriveSignInPresenter.kt
+++ app_manager/appmanager/src/main/java/app/editors/manager/storages/googledrive/mvp/presenters/GoogleDriveSignInPresenter.kt
@@ -1,26 +1,16 @@
 package app.editors.manager.storages.googledrive.mvp.presenters
 
-import android.accounts.Account
 import app.documents.core.account.CloudAccount
 import app.editors.manager.app.App
 import app.editors.manager.app.googleDriveLoginService
+import app.editors.manager.managers.utils.Constants
+import app.editors.manager.managers.utils.StorageUtils
+import app.editors.manager.storages.base.presenter.BaseStorageSignInPresenter
+import app.editors.manager.storages.base.view.BaseStorageSignInView
 import app.editors.manager.storages.googledrive.googledrive.login.GoogleDriveResponse
 import app.editors.manager.storages.googledrive.mvp.models.User
 import app.editors.manager.storages.googledrive.mvp.models.resonse.UserResponse
-import app.editors.manager.storages.googledrive.mvp.views.GoogleDriveSignInView
-import app.editors.manager.managers.utils.Constants
-import app.editors.manager.managers.utils.StorageUtils
-import app.editors.manager.mvp.presenters.base.BasePresenter
-import app.editors.manager.storages.base.presenter.BaseStorageSignInPresenter
-import app.editors.manager.storages.base.view.BaseStorageSignInView
-import io.reactivex.disposables.Disposable
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import lib.toolkit.base.R
 import lib.toolkit.base.managers.utils.AccountData
-import lib.toolkit.base.managers.utils.AccountUtils
 
 class GoogleDriveSignInPresenter: BaseStorageSignInPresenter<BaseStorageSignInView>() {
 
