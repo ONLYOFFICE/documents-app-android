48bdbfd5a9a7c2f15a9dbf9ad68c69408855a856
Michael Efremov <mikhail.efremov@onlyoffice.com>
2022-04-27 10:09:06 +0300

    [android] Increase build version

--- app_manager/appmanager/build.gradle.kts
+++ app_manager/appmanager/build.gradle.kts
@@ -60,7 +60,7 @@ android {
         manifestPlaceholders += mapOf()
         minSdk = AppDependency.MIN_SDK_VERSION
         targetSdk = AppDependency.TARGET_SDK_VERSION
-        versionCode = 365
+        versionCode = 366
         versionName = "5.4.0"
         multiDexEnabled = true
         applicationId = "com.onlyoffice.documents"
