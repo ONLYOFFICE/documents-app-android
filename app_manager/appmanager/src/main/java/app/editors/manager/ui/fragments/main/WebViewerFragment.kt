c2c4f3aa6e22198027f58a7b9dc74d18da387812
Michael Efremov <mikhail.efremov@onlyoffice.com>
2023-03-15 15:19:20 +0300

    [android] Add chrome to mobile user agent

--- app_manager/appmanager/src/main/java/app/editors/manager/ui/fragments/main/WebViewerFragment.kt
+++ app_manager/appmanager/src/main/java/app/editors/manager/ui/fragments/main/WebViewerFragment.kt
@@ -69,7 +69,7 @@ class WebViewerFragment : BaseAppFragment(), OnRefreshListener {
         private const val DESKTOP_USER_AGENT =
             "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36 AscAndroidWebView"
         private const val MOBILE_USER_AGENT =
-            "Mozilla/5.0 (Linux; U; Android 4.4; en-us; Nexus 4 Build/JOP24G) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 AscAndroidWebView"
+            "Mozilla/5.0 (Linux; U; Android 4.4; en-us; Nexus 4 Build/JOP24G) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 Chrome/37.0.2049.0 AscAndroidWebView"
 
         const val INTERFACE = "Android"
         const val KEY_EVENT = "event"
