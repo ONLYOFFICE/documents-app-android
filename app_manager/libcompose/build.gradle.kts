1d1769122d056fb4eba3fb091963bf8e4838b130
Sergey Artyushkin <srgartys@gmail.com>
2023-03-27 15:07:38 +0400

    [android] Add module

---
+++ app_manager/libcompose/build.gradle.kts
@@ -0,0 +1,55 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+}
+
+android {
+    buildToolsVersion = AppDependency.BUILD_TOOLS_VERSION
+    compileSdk = AppDependency.COMPILE_SDK_VERSION
+    defaultConfig {
+        minSdk = AppDependency.MIN_SDK_VERSION
+        targetSdk = AppDependency.TARGET_SDK_VERSION
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_11)
+        targetCompatibility(JavaVersion.VERSION_11)
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = Compose.versionCompiler
+    }
+}
+
+dependencies {
+    implementation(Compose.ui)
+    implementation(Compose.material)
+    implementation(Compose.preview)
+    debugImplementation(Compose.tooling)
+
+    implementation("androidx.core:core-ktx:1.7.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
