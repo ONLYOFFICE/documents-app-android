e2ca058c586c76e4f6f789669baae1c1b203ed18
Sergey Artyushkin <srgartys@gmail.com>
2023-04-27 13:52:10 +0400

    [android] Fix radio button alignment

--- app_manager/libcompose/src/main/java/lib/compose/ui/views/AppRadioItem.kt
+++ app_manager/libcompose/src/main/java/lib/compose/ui/views/AppRadioItem.kt
@@ -2,11 +2,21 @@ package lib.compose.ui.views
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.RadioButton
+import androidx.compose.material.RadioButtonDefaults
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,6 +33,7 @@ fun AppRadioItem(
         modifier = Modifier
             .fillMaxWidth()
             .height(48.dp)
+            .padding(start = 16.dp)
             .clickable { onCheck(!checked) }
     ) {
         Row(
@@ -34,7 +45,7 @@ fun AppRadioItem(
             RadioButton(
                 selected = checked,
                 onClick = { onCheck(!checked) },
-                modifier = Modifier.padding(start = 16.dp),
+                modifier = Modifier.size(dimensionResource(id = lib.toolkit.base.R.dimen.image_default_size)),
                 colors = RadioButtonDefaults.colors(selectedColor = MaterialTheme.colors.primary)
             )
             Text(
@@ -46,7 +57,7 @@ fun AppRadioItem(
                 modifier = Modifier.padding(horizontal = 16.dp)
             )
         }
-        AppDivider(startIndent = 16.dp)
+        AppDivider()
     }
 }
 
