bd75eb83a9dbaad284b8e0eccd68c48a32572253
Sergey Artyushkin <sergey.artyushkin@onlyoffice.com>
2023-09-26 18:34:58 +0400

    [android] Remove unused

--- app_manager/appmanager/src/main/java/app/editors/manager/mvp/views/storages/OneDriveSignInView.kt
+++
@@ -1,9 +0,0 @@
-package app.editors.manager.mvp.views.storages
-
-import app.editors.manager.mvp.views.base.BaseStorageSignInView
-import moxy.viewstate.strategy.OneExecutionStateStrategy
-import moxy.viewstate.strategy.StateStrategyType
-
-@StateStrategyType(OneExecutionStateStrategy::class)
-interface OneDriveSignInView : BaseStorageSignInView {
-}
