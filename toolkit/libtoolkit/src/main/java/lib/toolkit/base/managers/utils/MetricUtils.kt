c5db189b7875fd42d971ad6c928361192c7fa460
Sergey Artyushkin <sergey.artyushkin@onlyoffice.com>
2023-10-31 16:25:16 +0400

    [android] Add mm to inch

--- toolkit/libtoolkit/src/main/java/lib/toolkit/base/managers/utils/MetricUtils.kt
+++ toolkit/libtoolkit/src/main/java/lib/toolkit/base/managers/utils/MetricUtils.kt
@@ -5,33 +5,17 @@ import kotlin.math.round
 
 object MetricUtils {
 
-    @JvmStatic
-    fun ptToMm(value: Double): Double {
-        return value * 25.4 / 72
-    }
+    fun mmToPt(value: Double): Double = value * 72 / 25.4
 
-    @JvmStatic
-    fun mmToPt(value: Double): Double {
-        return value * 72 / 25.4
-    }
+    fun ptToMm(value: Double): Double = value * 25.4 / 72
 
-    @JvmStatic
-    fun mmToCm(value: Double): Double {
-        return value / 10
-    }
+    fun mmToCm(value: Double): Double = value / 10
 
-    @JvmStatic
-    fun cmToMm(value: Double): Double {
-        return value * 10
-    }
+    fun cmToMm(value: Double): Double = value * 10
 
-    @JvmStatic
-    fun roundToQuart(double: Double?): Double {
-        double?.let {
-            return round(it * 4) / 4.0
-        }
-        return 0.0
-    }
+    fun mmToInch(value: Double): Double = value * 0.03937
+
+    fun inchToMm(value: Double): Double = value / 0.03937
 
 }
 
