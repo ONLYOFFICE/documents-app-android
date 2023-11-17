80897f12b73fcbf2139cfd36759984afbba5ad67
Sergey Artyushkin <sergey.artyushkin@onlyoffice.com>
2023-11-17 13:21:39 +0300

    [android] Fix list items rtl

--- app_manager/libcompose/src/main/java/lib/compose/ui/views/AppListItem.kt
+++ app_manager/libcompose/src/main/java/lib/compose/ui/views/AppListItem.kt
@@ -19,18 +19,22 @@ import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import lib.compose.ui.addIfNotNull
 import lib.compose.ui.enabled
@@ -124,7 +128,7 @@ fun AppListItem(
 @Composable
 fun AppListItemsInteractivePreview() {
     var state by remember { mutableStateOf(false) }
-    var stepperState by remember { mutableStateOf(0) }
+    var stepperState by remember { mutableIntStateOf(0) }
 
     ManagerTheme {
         AppScaffold(topBar = {
@@ -190,4 +194,12 @@ fun AppListItemsInteractivePreview() {
 @Composable
 fun AppListItemsInteractivePreviewDark() {
     AppListItemsInteractivePreview()
+}
+
+@Preview(widthDp = 360, heightDp = 640)
+@Composable
+fun AppListItemsInteractivePreviewRTL() {
+    CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
+        AppListItemsInteractivePreview()
+    }
 }
