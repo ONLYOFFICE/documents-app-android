9b42627b444c89711f8585a5e481fd2ada3ff186
Sergey Artyushkin <sergey.artyushkin@onlyoffice.com>
2023-11-27 18:15:41 +0300

    [android] Fix fade animation

--- app_manager/libcompose/src/main/java/lib/compose/ui/views/Animation.kt
+++ app_manager/libcompose/src/main/java/lib/compose/ui/views/Animation.kt
@@ -1,6 +1,12 @@
 package lib.compose.ui.views
 
-import androidx.compose.animation.*
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -13,8 +19,8 @@ fun AnimatedVisibilityVerticalFade(
     AnimatedVisibility(
         modifier = modifier,
         visible = visible,
-        enter = fadeIn() + expandVertically(),
-        exit = fadeOut() + shrinkVertically(),
+        enter = fadeIn(spring()) + expandVertically(),
+        exit = fadeOut(spring()) + shrinkVertically(),
         content = content
     )
 }
