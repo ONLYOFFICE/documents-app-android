283282f0b1dbdb5f8c9161658c61b0741e6b005b
Michael Efremov <mikhail.efremov@onlyoffice.com>
2023-11-30 17:05:31 +0300

    [android] Update dependencies

--- app_manager/build.gradle.kts
+++ app_manager/build.gradle.kts
@@ -14,7 +14,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("com.android.tools.build:gradle:8.1.4")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Kotlin.version}")
         classpath("com.google.gms:google-services:4.4.0")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
