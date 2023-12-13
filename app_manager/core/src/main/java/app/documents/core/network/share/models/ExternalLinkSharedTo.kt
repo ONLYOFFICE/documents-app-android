d30284c0505e92e6fb93a033f0a837de8e3e182e
Sergey Artyushkin <sergey.artyushkin@onlyoffice.com>
2023-12-13 20:37:04 +0700

    [android] Fix showing save dialog

--- app_manager/core/src/main/java/app/documents/core/network/share/models/ExternalLinkSharedTo.kt
+++ app_manager/core/src/main/java/app/documents/core/network/share/models/ExternalLinkSharedTo.kt
@@ -1,6 +1,7 @@
 package app.documents.core.network.share.models
 
 import kotlinx.serialization.Serializable
+import lib.toolkit.base.managers.utils.TimeUtils
 
 @Serializable
 data class ExternalLinkSharedTo(
@@ -13,5 +14,44 @@ data class ExternalLinkSharedTo(
     val isExpired: Boolean,
     val primary: Boolean,
     val requestToken: String,
-    val expirationDate: String? // "2023-12-08T00:00:00.0000000+03:00"
-)
+    val expirationDate: String?
+) {
+
+    private fun dateToLong(): Long {
+        expirationDate?.let { date ->
+            return TimeUtils.DEFAULT_FORMAT.parse(date)?.time ?: 0
+        } ?: return 0
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + title.hashCode()
+        result = 31 * result + shareLink.hashCode()
+        result = 31 * result + linkType
+        result = 31 * result + (password?.hashCode() ?: 0)
+        result = 31 * result + denyDownload.hashCode()
+        result = 31 * result + isExpired.hashCode()
+        result = 31 * result + primary.hashCode()
+        result = 31 * result + requestToken.hashCode()
+        result = 31 * result + dateToLong().toInt()
+        return result
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ExternalLinkSharedTo
+
+        if (id != other.id) return false
+        if (title != other.title) return false
+        if (shareLink != other.shareLink) return false
+        if (linkType != other.linkType) return false
+        if (password != other.password) return false
+        if (denyDownload != other.denyDownload) return false
+        if (isExpired != other.isExpired) return false
+        if (primary != other.primary) return false
+        if (requestToken != other.requestToken) return false
+        return dateToLong().toInt() == other.dateToLong().toInt()
+    }
+}
