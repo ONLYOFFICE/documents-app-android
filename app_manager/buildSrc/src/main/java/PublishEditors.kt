1e069e01f344737c98af1bfd346b30411ee02853
Michael Efremov <mikhail.efremov@onlyoffice.com>
2024-04-17 12:05:38 +0300

    [android] Change version

--- app_manager/buildSrc/src/main/java/PublishEditors.kt
+++ app_manager/buildSrc/src/main/java/PublishEditors.kt
@@ -4,7 +4,7 @@ object PublishEditors {
     val publishUrl = "https://github.com/ONLYOFFICE/editors-android-packages"
 
     val groupId = "com.onlyoffice.android"
-    val version = "1.0.0"
+    val version = "0.0.1"
 
     val x2tId = "x2t"
     val cellsId = "cells"
