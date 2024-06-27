4b02ce417e699d173642e909c9bd41014b184fc3
Michael Efremov <mikhail.efremov@onlyoffice.com>
2024-06-27 10:48:10 +0300

    [android] Update version name

--- app_manager/appmanager/build.gradle.kts
+++ app_manager/appmanager/build.gradle.kts
@@ -65,7 +65,7 @@ android {
         minSdk = AppDependency.MIN_SDK_VERSION
         targetSdk = AppDependency.TARGET_SDK_VERSION
         versionCode = 551
-        versionName = "8.1.0"
+        versionName = "8.1.1"
         multiDexEnabled = true
         applicationId = "com.onlyoffice.documents"
 
