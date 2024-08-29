d9a10a1daeba9940fc5067cb4149a1a29eea21f0
macOS Build Slave <builder@macOSs-Mac-mini.local>
2024-08-29 21:00:36 +0300

    Version bumped to v8.2.0(570)

--- app_manager/appmanager/build.gradle.kts
+++ app_manager/appmanager/build.gradle.kts
@@ -64,7 +64,7 @@ android {
         manifestPlaceholders += mapOf()
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 569
+        versionCode = 570
         versionName = "8.2.0"
         multiDexEnabled = true
         applicationId = "com.onlyoffice.documents"
