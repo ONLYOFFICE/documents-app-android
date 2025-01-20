1e11e82ed87c971de22a88f9c2d0eb3f98d6ec44
Sergey Artyushkin <sergey.artyushkin@onlyoffice.com>
2025-01-20 16:54:48 +0300

    [android] Fix workspace strings

--- app_manager/core/model/src/main/java/app/documents/core/model/cloud/UserType.kt
+++ app_manager/core/model/src/main/java/app/documents/core/model/cloud/UserType.kt
@@ -1,8 +1,8 @@
 package app.documents.core.model.cloud
 
 sealed class UserType {
-    data object DocSpaceOwner : UserType()
-    data object DocSpaceAdmin : UserType()
+    data object Owner : UserType()
+    data object Admin : UserType()
     data object RoomAdmin : UserType()
     data object User : UserType()
     data object Guest : UserType()
