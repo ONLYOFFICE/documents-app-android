1aaad20320eae5f92f847cc0a50cb0ee3e7b914d
AinSl <ain.sln.ai21@gmail.com>
2025-04-14 12:11:44 +0400

    [android] Add comment editing UI and logic

---
+++ app_manager/appmanager/src/main/java/app/editors/manager/ui/fragments/main/versionhistory/EditCommentScreen.kt
@@ -0,0 +1,135 @@
+package app.editors.manager.ui.fragments.main.versionhistory
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import app.editors.manager.R
+import app.editors.manager.viewModels.main.BaseEvent
+import app.editors.manager.viewModels.main.EditCommentEvent
+import app.editors.manager.viewModels.main.VersionEditCommentViewModel
+import lib.compose.ui.views.AppScaffold
+import lib.compose.ui.views.AppTopBar
+import lib.compose.ui.views.TopAppBarAction
+
+@Composable
+fun EditCommentScreen(
+    onSuccess: () -> Unit,
+    onBack: () -> Unit
+){
+    val viewModel: VersionEditCommentViewModel = viewModel(factory = VersionEditCommentViewModel.Factory)
+    val scaffoldState = rememberScaffoldState()
+    val state by viewModel.uiState.collectAsState()
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val focusManager = LocalFocusManager.current
+
+    LaunchedEffect(viewModel.events) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){
+            viewModel.events.collect { event ->
+                when(event){
+                    is BaseEvent.ShowMessage -> {
+                        scaffoldState.snackbarHostState.showSnackbar(event.msg)
+                    }
+                    is EditCommentEvent.UpdateHistory -> {
+                        onSuccess()
+                    }
+                }
+            }
+        }
+    }
+
+    AppScaffold(
+        topBar = {
+            Column {
+                AppTopBar(
+                    title = stringResource(R.string.version_edit_comment_title),
+                    backListener = onBack,
+                    isClose = true,
+                    actions = {
+                        TopAppBarAction(
+                            icon = R.drawable.drawable_ic_done,
+                            enabled = !state.isLoading && state.comment.isNotBlank(),
+                            onClick = {
+                                focusManager.clearFocus()
+                                viewModel.saveComment()
+                            }
+                        )
+                    }
+                )
+                if (state.isLoading) {
+                    LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
+                }
+            }
+        },
+        scaffoldState = scaffoldState,
+        useTablePaddings = false
+    ) {
+        EditCommentScreenContent(
+            comment = state.comment,
+            onValueChange = viewModel::onCommentChange
+        )
+    }
+}
+
+@Composable
+fun EditCommentScreenContent(
+    comment: String,
+    onValueChange: (String) -> Unit
+){
+    val focusRequester = remember { FocusRequester() }
+    val textFieldValue = remember {
+        mutableStateOf(TextFieldValue(
+            text = comment, selection = TextRange(comment.length)
+        ))
+    }
+
+    Box(Modifier.fillMaxSize().padding(horizontal = 16.dp, vertical = 8.dp)){
+        TextField(
+            value = textFieldValue.value,
+            onValueChange = {
+                textFieldValue.value = it
+                onValueChange(it.text)
+            },
+            label = { Text(stringResource(R.string.edit_comment_label)) },
+            singleLine = true,
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = MaterialTheme.colors.onSurface,
+                disabledTextColor = Color.Transparent,
+                backgroundColor = Color.Transparent,
+                focusedIndicatorColor = MaterialTheme.colors.primary,
+                unfocusedIndicatorColor = Color.Gray,
+                disabledIndicatorColor = Color.Gray,
+                focusedLabelColor = MaterialTheme.colors.primary
+            ),
+            modifier = Modifier.fillMaxWidth()
+                .focusRequester(focusRequester)
+        )
+        LaunchedEffect(Unit) { focusRequester.requestFocus() }
+    }
+}
---
+++ app_manager/appmanager/src/main/java/app/editors/manager/viewModels/main/VersionEditCommentViewModel.kt
@@ -0,0 +1,84 @@
+package app.editors.manager.viewModels.main
+
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.navigation.toRoute
+import app.documents.core.providers.CloudFileProvider
+import app.editors.manager.R
+import app.editors.manager.app.App
+import app.editors.manager.ui.fragments.main.versionhistory.VersionHistoryFragment
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import lib.toolkit.base.managers.tools.ResourcesProvider
+
+sealed interface EditCommentEvent : BaseEvent {
+    data object UpdateHistory : EditCommentEvent
+}
+
+data class EditCommentState(
+    val fileId: String,
+    val version: Int,
+    val comment: String,
+    val isLoading: Boolean
+)
+
+class VersionEditCommentViewModel(
+    private val cloudFileProvider: CloudFileProvider,
+    private val resourceProvider: ResourcesProvider,
+    savedStateHandle: SavedStateHandle
+) : BaseEventViewModel(resourceProvider) {
+
+    private val data = savedStateHandle.toRoute<VersionHistoryFragment.Screens.EditComment>()
+    private val _uiState = MutableStateFlow(
+        EditCommentState(
+            fileId = data.fileId,
+            version = data.version,
+            comment = data.comment,
+            isLoading = false
+        )
+    )
+    val uiState = _uiState.asStateFlow()
+
+    fun saveComment(){
+        viewModelScope.launch {
+            with(_uiState.value){
+                cloudFileProvider.editVersionComment(fileId, version, comment)
+                    .onStart { _uiState.update { it.copy(isLoading = true) } }
+                    .onCompletion { _uiState.update { it.copy(isLoading = false) } }
+                    .collect { result ->
+                        if (result.isSuccess) {
+                            _events.send(EditCommentEvent.UpdateHistory)
+                            sendMessage(R.string.edit_comment_success)
+                        }
+                        else sendMessage(R.string.error_edit_comment)
+                    }
+            }
+        }
+    }
+
+    fun onCommentChange(newComment: String){
+        _uiState.update { it.copy(comment = newComment) }
+    }
+
+    companion object {
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val appComponent = (this[APPLICATION_KEY] as App).appComponent
+                VersionEditCommentViewModel(
+                    cloudFileProvider = appComponent.cloudFileProvider,
+                    resourceProvider = appComponent.resourcesProvider,
+                    savedStateHandle = createSavedStateHandle()
+                )
+            }
+        }
+    }
+}
