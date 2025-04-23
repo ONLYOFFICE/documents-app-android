2bc0a06370a35023fa22954f497afd09318b044d
AinSl <ain.sln.ai21@gmail.com>
2025-04-23 17:45:59 +0400

    [android] Fix bug 74062

--- app_manager/appmanager/src/main/java/app/editors/manager/mvp/presenters/main/DocsCloudPresenter.kt
+++ app_manager/appmanager/src/main/java/app/editors/manager/mvp/presenters/main/DocsCloudPresenter.kt
@@ -985,10 +985,7 @@ class DocsCloudPresenter(private val account: CloudAccount) : DocsBasePresenter<
             roomProvider.lockFile(id = file.id, lock = !file.isLocked)
                 .collect { result ->
                     when(result){
-                        is Result.Success -> {
-                            file.isLocked = !file.isLocked
-                            viewState.onUpdateItemState()
-                        }
+                        is Result.Success -> refresh()
                         is Result.Error -> fetchError(result.exception)
                     }
                 }
