66eb886c65540dd271b0ac48f5bde93bfbba0825
michael-efremov <michael.efremov@onlyoffice.com>
2025-06-20 17:45:20 +0300

    [android] Fix bug 75421

--- app_manager/appmanager/src/main/java/app/editors/manager/mvp/presenters/main/DocsCloudPresenter.kt
+++ app_manager/appmanager/src/main/java/app/editors/manager/mvp/presenters/main/DocsCloudPresenter.kt
@@ -1099,6 +1099,7 @@ class DocsCloudPresenter(private val account: CloudAccount) : DocsBasePresenter<
     }
 
     fun interruptConversion(): Boolean {
+        if (conversionJob?.isCompleted == true) return false
         val cancelled = conversionJob?.isCancelled == true // conversionJob == null || isCancelled
         conversionJob?.cancel()
         return cancelled
