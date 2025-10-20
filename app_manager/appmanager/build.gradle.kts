b89b2b7b7aaee528727e9df964dcd24d7243ce7d
builder <builder@Macmini3043.local>
2025-10-20 13:53:29 +0300

    Version bumped to v9.1.0(658)

--- app_manager/appmanager/build.gradle.kts
+++ app_manager/appmanager/build.gradle.kts
@@ -68,7 +68,7 @@ android {
         manifestPlaceholders += mapOf()
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 657
+        versionCode = 658
         versionName = "9.1.0"
         multiDexEnabled = true
         applicationId = "com.onlyoffice.documents"
