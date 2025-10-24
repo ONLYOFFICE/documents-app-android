bd9f5b7a92af00d8082406d82504bf46904d8a98
michael-efremov <michael.efremov@onlyoffice.com>
2025-10-24 15:11:03 +0300

    [android] Update "What's New" for v9.1

--- app_manager/appmanager/build.gradle.kts
+++ app_manager/appmanager/build.gradle.kts
@@ -68,7 +68,7 @@ android {
         manifestPlaceholders += mapOf()
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 659
+        versionCode = 660
         versionName = "9.1.0"
         multiDexEnabled = true
         applicationId = "com.onlyoffice.documents"
