361b889db6f8846f089db9d2c0c9654b4066351d
AinSl <aynur.nurusheva@onlyoffice.com>
2025-10-29 18:48:26 +0400

    [android] Fix pagination

--- app_manager/appmanager/src/main/java/app/editors/manager/ui/views/recyclers/LoadingScroll.kt
+++ app_manager/appmanager/src/main/java/app/editors/manager/ui/views/recyclers/LoadingScroll.kt
@@ -1,35 +1,25 @@
-package app.editors.manager.ui.views.recyclers;
+package app.editors.manager.ui.views.recyclers
 
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+abstract class LoadingScroll : RecyclerView.OnScrollListener() {
 
-abstract public class LoadingScroll extends RecyclerView.OnScrollListener {
-
-    private static final int VISIBLE_THRESHOLD = 5;
-    private boolean mIsDown = false;
-
-    @Override
-    public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-        super.onScrollStateChanged(recyclerView, newState);
+    override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+        super.onScrollStateChanged(recyclerView, newState)
         if (newState == RecyclerView.SCROLL_STATE_DRAGGING) {
-            final LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
-            final int visibleItemCount = layoutManager.getChildCount();
-            final int totalItemCount = layoutManager.getItemCount();
-            final int firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition();
-            if (mIsDown && visibleItemCount + firstVisibleItemPosition >= totalItemCount - VISIBLE_THRESHOLD) {
-                mIsDown = false;
-                onListEnd();
+            val layoutManager = (recyclerView.layoutManager as? LinearLayoutManager) ?: return
+            val totalItemCount = layoutManager.itemCount
+            val lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
+            if (lastVisibleItemPosition >= totalItemCount - VISIBLE_THRESHOLD) {
+                onListEnd()
             }
         }
     }
 
-    @Override
-    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-        super.onScrolled(recyclerView, dx, dy);
-        mIsDown = dy > 0;
+    abstract fun onListEnd()
+
+    companion object {
+        private const val VISIBLE_THRESHOLD = 15
     }
-
-    abstract public void onListEnd();
-
 }
