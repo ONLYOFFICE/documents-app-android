4f210e63cbf1ac5824b6e2e8b8aae6e33dccfe5e
michael-efremov <michael.efremov@onlyoffice.com>
2025-11-28 10:57:24 +0300

    [android] Refactor resources

--- app_manager/settings.gradle.kts
+++ app_manager/settings.gradle.kts
@@ -34,6 +34,7 @@ if (shouldIncludeEditors() == true) {
     includeEditorModule("../../core-ext/word_android/libdocs", ":libdocs")
 //    includeEditorModule("../../core-ext/slide_android/libslides", ":libslides")
     includeEditorModule("../../document-android-editors/editors_base/libgeditors", ":libgeditors")
+    includeEditorModule("../../document-android-editors/editors_base/libresources", ":libresources")
 //    includeEditorModule("../../document-android-editors/editors_cells/libgcells", ":libgcells")
     includeEditorModule("../../document-android-editors/editors_docs/libgdocs", ":libgdocs")
 //    includeEditorModule("../../document-android-editors/editors_slides/libgslides", ":libgslides")
