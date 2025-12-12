a3a70ea0aae3a7f1fc1911fd8e6e3ba3afb80794
michael-efremov <michael.efremov@onlyoffice.com>
2025-12-12 15:08:43 +0300

    [android] Bump version name to 9.2.1

--- app_manager/appmanager/build.gradle.kts
+++ app_manager/appmanager/build.gradle.kts
@@ -69,7 +69,7 @@ android {
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
         versionCode = 673
-        versionName = "9.2.0"
+        versionName = "9.2.1"
         multiDexEnabled = true
         applicationId = "com.onlyoffice.documents"
 
